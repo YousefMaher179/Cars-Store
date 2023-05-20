85d338b82eefdf193b6ae7147a2f1c4ca0b1924b
YousefMaher179 <ym1233445@gmail.com>
2023-05-20 20:58:27 +0300

    Cars-Store

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Hero from "./components/Header/Hero";
+import About from "./components/Layouts/About/About";
+import Services from "./components/Layouts/Services/Services";
+import CarsList from "./components/Layouts/Cars/CarsList";
+import Model from "./components/Layouts/Models/Model";
+import Testimonials from "./components/Layouts/Testimonials/Testimonials";
+import CartProvider from "./Store/cart-context";
+import Companies from "./components/Layouts/Companies/Companies";
+import Footer from "./components/Layouts/Footer/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <CartProvider>
+      <Hero />
+      <About />
+      <Services />
+      <CarsList />
+      <Model />
+      <Testimonials />
+      <Companies />
+      <Footer />
+    </CartProvider>
   );
 }
 
